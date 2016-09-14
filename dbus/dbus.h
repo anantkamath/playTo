58a4ac80e3cfa5af42849f4b6c642a34fcc7379f
Anant Kamath <kamathanant@gmail.com>
2016-09-14 17:26:05 +0530

    Regenerated dbus bindings to dleyna 0.5.0

--- dbus/dbus.h
+++ dbus/dbus.h
@@ -5,7 +5,8 @@
 #ifndef DBUS_H
 #define DBUS_H
 
-#define ROOT_OBJECT_PATH "/com/intel/dLeynaRenderer"
-#define RENDERER_SERVICE "com.intel.dleyna-renderer"
+#define ROOT_OBJECT_PATH QLatin1String("/com/intel/dLeynaRenderer")
+#define RENDERER_SERVICE QLatin1String("com.intel.dleyna-renderer")
+#define DBUS_PROPS_INTERFACE QLatin1String("org.freedesktop.DBus.Properties")
 
 #endif // DBUS_H
